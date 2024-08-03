30d8f1a3b3d06f2791d45eddd3e67c7aff0c16ab
Juan Mosquera <jdmg206@gmail.com>
2024-08-02 21:54:20 -0500

    Realice el punto 13

---
+++ trece.py
@@ -0,0 +1,13 @@
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1/num2
+
+print("La suma, resta, multiplicacion y division, son respectivamente: ")
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
